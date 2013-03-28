90c0bc0ae16cc8ce12af1f81f462851fdbadb5a3
Jimmy Liang <lyh@yazotech.com>
2013-03-29 00:57:26 +0800

    add config host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Omei::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -64,7 +64,7 @@ Omei::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => ENV["HOST_NAME"] }
+  config.action_mailer.default_url_options = { :host => 'aetravelusa.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
